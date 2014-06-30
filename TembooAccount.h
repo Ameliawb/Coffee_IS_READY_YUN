78503222e2f60063944c1607c693b72024e87d00
Amelia WInger-Bearskin <ameliawb@nyu.edu>
2014-06-30 10:48:11 -0400

    Create TembooAccount.h

---
+++ TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "ameliaXXXXXXX"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "XXXXX"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "XXXXXXXXXXXXX"  // your Temboo app key
